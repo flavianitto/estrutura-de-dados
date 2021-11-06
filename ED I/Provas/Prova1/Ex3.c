11dd2a3b105c4c81a44958e7965ff94f5c36bd8a
flavianitto <85723447+flavianitto@users.noreply.github.com>
2021-11-06 20:29:39 -0300

    prova 1

--- ED I/Provas/Prova1/Ex3.c
+++
@@ -1,5 +0,0 @@
-/*
-Escreva uma função, em C/C++, que pesquisa recursivamente em uma lista encadeada um valor específico. A função deve retornar 
-um ponteiro para o valor se ele for encontrado, caso contrário, deve ser retornado um valor nulo. Use a sua função em um programa
-de teste que cria uma lista de inteiros. O programa deve solicitar ao usuário um valor a ser procurado na lista. 
-*/
